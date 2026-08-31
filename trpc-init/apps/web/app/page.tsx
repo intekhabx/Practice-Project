40f945947f4f16c615f8bcbcc684549c8ed30fe7
itz-devilx <intekhab118211989@gmail.com>
2026-08-31 20:35:31 +0530

    feat: integrate tanstack query with trpc and add Providers component for context

--- trpc-init/apps/web/app/page.tsx
+++ trpc-init/apps/web/app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
-import { trpc } from '../trpc/client'
+import { trpc, trpcClient } from '../trpc/client'
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 
 interface ITodo {
@@ -18,13 +19,29 @@ function page() {
 
   useEffect(() => {
     async function getAllTodosFromBackend(){
-      const data = await trpc.getAllTodos.query();
+      const data = await trpcClient.getAllTodos.query();
       // console.log(data);
       setTodos(data);
     }
 
     getAllTodosFromBackend();
   }, [])
+
+
+  // new method to use the tanstack query with trpc
+  // const {data: allTodos} = useQuery(trpc.getAllTodos.queryOptions());
+  // console.log("new-calling",allTodos);
+
+  const {data: oneTodo} = useQuery(trpc.getTodoById.queryOptions("2"));
+  console.log(oneTodo);
+
+
+  // useMutation usage but dont call deleteTodo.mutate() direct on component mount call on button click or some event
+  const deleteTodo = useMutation(trpc.deleteTodo.mutationOptions());
+  // deleteTodo.mutate({id: "1"});
+
+  const addTodo = useMutation(trpc.addTodo.mutationOptions());
+  // addTodo.mutate({id: 5, title: "new titile", desc: "new desc"})
 
 
   return (
--- trpc-init/apps/web/trpc/client.ts
+++ trpc-init/apps/web/trpc/client.ts
@@ -1,12 +1,30 @@
 import { createTRPCClient, httpBatchLink, loggerLink } from '@trpc/client';
 import type { AppRouter } from '../../api/src/trpc/router';
+import { QueryClient } from '@tanstack/react-query';
+import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
 
 
-export const trpc = createTRPCClient<AppRouter>({
+export const queryClient = new QueryClient();
+
+export const trpcClient = createTRPCClient<AppRouter>({
   links: [
     loggerLink(),
     httpBatchLink({
       url: 'http://localhost:5000/trpc',
+
+      // this is for the cors, every fetch all includes credetials: "include"
+      fetch(url, options){
+        return fetch(url, {
+          ...options,
+          credentials: "include"
+        })
+      }
     }),
   ],
 });
+
+
+export const trpc = createTRPCOptionsProxy<AppRouter>({
+  client: trpcClient,
+  queryClient,
+})
